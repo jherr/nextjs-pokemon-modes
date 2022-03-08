56ce77a50272e22f248f56792a77a0575241a7b8
Jack Herrington <jherr@pobox.com>
2022-03-07 21:02:51 -0800

    Update revalidate.js

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,4 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// I didn't follow the security pattern suggested in the NextJS documentation but you should:
+//
+// https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
 
 export default async function handler(req, res) {
   for (const url of req.body) {
